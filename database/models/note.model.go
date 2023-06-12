002cd58bbf55d648900a4d5d37d1d31e168470f5
pius <piusrestiantoro2@gmail.com>
2023-06-12 11:12:41 +0800

    note model modified

--- database/models/note.model.go
+++ database/models/note.model.go
@@ -4,7 +4,7 @@ import "time"
 
 type Note struct {
 	NoteID    string    `gorm:"primarykey; type:uuid; default:uuid_generate_v4()" json:"id,omitempty" valid:"-"`
-	Title     string    `json:"title,omitempty" valid:"required~Title cannot be empty"`
+	Title     string    `json:"title,omitempty" valid:"-"`
 	Note      string    `gorm:"default:text" json:"note,omitempty" valid:"-"`
 	CreatedAt time.Time `json:"created_at" valid:"-"`
 	UpdatedAt time.Time `json:"updated_at" valid:"-"`
